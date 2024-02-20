375835a73c426c9efd47ea1f92442bf6524031e7
Dawid Federowicz <dawid.federowicz@omegacode.pl>
2024-02-20 14:23:05 +0100

    fix - array

--- evaluation-reset.sh
+++ evaluation-reset.sh
@@ -1,20 +1,25 @@
-for product in IntelliJIdea WebStorm DataGrip PhpStorm CLion PyCharm GoLand RubyMine; do
+#!/bin/bash
+
+products=(IntelliJIdea WebStorm DataGrip PhpStorm CLion PyCharm GoLand RubyMine)
+
+for product in ${products[@]}
+do
   echo "Resetting trial for $product"
 
-  echo "removing evaluation key..."
+  echo "removing key..."
   rm -rf ~/.config/$product*/eval
 
-  # Above path not working on latest version. Fixed below
+  # Fix for latest version
   rm -rf ~/.config/JetBrains/$product*/eval
 
   echo "removing all evlsprt properties in options.xml..."
   sed -i 's/evlsprt//' ~/.config/$product*/options/other.xml
 
-  # Above path not working on latest version. Fixed below
+  # Fix for latest version
   sed -i 's/evlsprt//' ~/.config/JetBrains/$product*/options/other.xml
-
-  echo
 done
 
 echo "removing userPrefs files..."
 rm -rf ~/.java/.userPrefs
+
+echo "DONE"
